29227ee6ad1f21f9e5194f61d25bde99c15c111c
Alla Roman <alla.roman.mail@gmail.com>
2018-02-27 16:13:34 +0100

    github

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.json({welcome: 'Article API'});
+});
+
+module.exports = router;
